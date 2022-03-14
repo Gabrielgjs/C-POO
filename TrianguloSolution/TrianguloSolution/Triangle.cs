4cdedc777e5b06eaa7c98cff88394c34e871a2f7
Gabrielgjs <gjsgabriel83@gmail.com>
2022-03-14 10:28:20 -0300

    Refatorando Classe Triangle com metodo Area

--- TrianguloSolution/TrianguloSolution/Triangle.cs
+++ TrianguloSolution/TrianguloSolution/Triangle.cs
@@ -12,5 +12,12 @@ namespace TrianguloSolution
         public double B;
         public double C;
 
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
     }
 }
