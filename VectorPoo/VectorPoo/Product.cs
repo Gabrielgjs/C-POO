59083e09f97d2cfdeafdea41d62baed24e48cf38
gjsgabriel <gjsgabriel83@gmail.com>
2022-04-18 14:05:29 -0300

    vetor parte2

---
+++ VectorPoo/VectorPoo/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VectorPoo
+{
+    class Product
+    {
+        public string Nmae  { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ VectorPoo/VectorPoo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace VectorPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Nmae = name, Price = price };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i].Price;
+            }
+
+            double media = soma / n;
+            Console.WriteLine("Preço médio =" + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
