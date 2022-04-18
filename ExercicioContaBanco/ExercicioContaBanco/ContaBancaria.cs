4373d2695e068fa7c457f15d157f76745ac06994
gjsgabriel <gjsgabriel83@gmail.com>
2022-04-18 13:46:08 -0300

    vetor parte 1

--- ExercicioContaBanco/ExercicioContaBanco/ContaBancaria.cs
+++ ExercicioContaBanco/ExercicioContaBanco/ContaBancaria.cs
@@ -19,9 +19,9 @@ namespace ExercicioContaBanco
             Titular = titular;
         }
 
-        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
         {
-            Saldo = saldo;
+            Deposito(depositoInicial);
         }
 
         public void Deposito(double quantia)
---
+++ Vector/Vector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double media = soma / n;
+            Console.WriteLine("Altura média = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
