373e7d301ab33680b684bb2390aa23df6972f7b6
gjsgabriel <gjsgabriel83@gmail.com>
2022-04-19 16:36:27 -0300

    Modificador de parametro params

---
+++ ModificadorDeParametros/ModificadorDeParametros/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModificadorDeParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Calculadora.Soma(2, 2);
+            int b = Calculadora.Soma(2, 2, 4);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
