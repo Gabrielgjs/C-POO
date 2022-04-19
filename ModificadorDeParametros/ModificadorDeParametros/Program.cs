34785490c75c7ff6d36f2a8cf7f82b6fab309f43
gjsgabriel <gjsgabriel83@gmail.com>
2022-04-19 16:53:52 -0300

    modificador Ref

--- ModificadorDeParametros/ModificadorDeParametros/Program.cs
+++ ModificadorDeParametros/ModificadorDeParametros/Program.cs
@@ -8,6 +8,7 @@ namespace ModificadorDeParametros
         {
             int a = Calculadora.Soma(2, 2);
             int b = Calculadora.Soma(2, 2, 4);
+            //int c = Calculadora.Soma(new int[] { 5, 5});
             Console.WriteLine(a);
             Console.WriteLine(b);
         }
---
+++ ModificadorParametroRefOut/ModificadorParametroRefOut/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorParametroRefOut
+{
+    class Calculadora
+    {
+        public static void Triplicar(ref int x)
+        {
+            x = x * 3;
+        }
+    }
+}
